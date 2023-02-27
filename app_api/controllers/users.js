737aecb3ca0d872a9ffd95b13d94795052152b6c
Henry Salazar <hsalazar@estud.usfq.edu.ec>
2023-02-26 19:34:26 -0500

    add app_api routes list and read 26/02/2023

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -1,4 +1,5 @@
-
+const mongoose = require('mongoose');
+const users = mongoose.model('user');
 
 
 const userCreate = (req, res) => {
@@ -7,22 +8,54 @@ const userCreate = (req, res) => {
         .json({
             "status": "created successfully"
         });
+    
 }
 
 const userList = (req, res) => {
-    res
-        .status(200)
-        .json({
-            "status": "listed successfully"
-        });
+    users
+        .find()
+        .exec((err, objetoUsuario)=>{
+            if(!objetoUsuario){
+                console.log(`usuarios no encontrados)`);
+                return res
+                    .status(404)
+                    .json({"mensaje" : "usuarios no encontrados"})
+            }else if(err){
+                console.log(`usuarios tiene errores)`);
+                return res
+                    .status(404)
+                    .json(err);
+            }
+            res
+                .status(200)
+                .json(objetoUsuario);
+        })
+    //res
+    //    .status(200)
+    //    .json({
+    //        "status": "listed successfully"
+    //    });
 }
 
 const userRead = (req, res) => {
-    res
-        .status(200)
-        .json({
-            "status": "read action successfully"
-        });
+    users
+        .findById(req.params.userid)
+        .exec((err, objetoUsuario)=>{
+            if(!objetoUsuario){
+                console.log(`usuario especificado: ${req.params.userid} no encontrado)`);
+                return res
+                    .status(404)
+                    .json({"mensaje" : "usuario no encontrado"})
+            }else if(err){
+                console.log(`usuario especificado: ${req.params.userid} tiene errores)`);
+                return res
+                    .status(404)
+                    .json(err);
+            }
+            res
+            .status(200)
+            .json(objetoUsuario);
+        })
 }
 
 const userUpdate = (req, res) => {
